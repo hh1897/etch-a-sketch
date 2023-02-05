288f4301d33792ebff812537b7407e4ff4645e53
hh1897 <64330680+hh1897@users.noreply.github.com>
2023-02-05 17:15:08 +0000

    Started grid function

--- etch.js
+++ etch.js
@@ -1,1 +1,10 @@
 // JavaScript source code
+
+
+function grid {
+    for (i = 0; i <= 16; i++) {
+        const box = document.createElement("div");
+        box.className = "box";
+        document.body.appendChild(box);
+    }
+}
