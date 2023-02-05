177011c8f687952cacff5625ebb616bef6269a52
hh1897 <64330680+hh1897@users.noreply.github.com>
2023-02-05 18:38:27 +0000

    Able to make a 16x16 grid
    There is a fault. When the button is first pressed it makes an empty row div.

--- etch.js
+++ etch.js
@@ -2,9 +2,26 @@
 
 
 function grid() {
-    for (i = 0; i <= 16; i++) {
-        const box = document.createElement("div");
-        box.className = "box";
-        document.body.appendChild(box);
+
+    let rowName = "row";
+    let rowCount = 0;
+    const grid = document.createElement("div");
+    grid.id = "grid";
+    document.body.appendChild(grid);
+
+    for (m = 0; m <= 16; m++) {
+
+        const row = document.createElement("div");
+        row.className = "row";
+        document.getElementById("grid").appendChild(row)[m];
+        rowCount++;
+
+        // Create row
+        for (i = 0; i <= 16; i++) {
+            const box = document.createElement("div");
+            box.className = "box";
+            document.getElementsByClassName("row")[rowCount].appendChild(box)[i];
+            
+        }
     }
 }
