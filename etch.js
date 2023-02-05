dcacfa12ecb90175f12bcded554e9a0793e2e9fe
hh1897 <64330680+hh1897@users.noreply.github.com>
2023-02-05 17:21:08 +0000

    created css file

--- etch.js
+++ etch.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 
 
-function grid {
+function grid() {
     for (i = 0; i <= 16; i++) {
         const box = document.createElement("div");
         box.className = "box";
