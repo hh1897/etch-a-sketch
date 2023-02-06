c53627abcc0b8a03ad20f6496179bb89a1d87a9e
hh1897 <64330680+hh1897@users.noreply.github.com>
2023-02-06 13:01:05 +0000

    Created user input box

--- etch.js
+++ etch.js
@@ -1,27 +1,34 @@
 // JavaScript source code
 
+const numOfSquares = document.getElementById("userInput").value;
 
-function grid() {
-
+function grid(numOfSquares) {
+    
     let rowName = "row";
     let rowCount = 0;
     const grid = document.createElement("div");
     grid.id = "grid";
-    document.body.appendChild(grid);
+    document.getElementById("gridParent").appendChild(grid);
 
-    for (m = 0; m <= 16; m++) {
+    for (m = 0; m < numOfSquares; m++) {
 
         const row = document.createElement("div");
         row.className = "row";
         document.getElementById("grid").appendChild(row)[m];
-        rowCount++;
+        
 
         // Create row
-        for (i = 0; i <= 16; i++) {
+        for (i = 0; i < numOfSquares; i++) {
             const box = document.createElement("div");
             box.className = "box";
             document.getElementsByClassName("row")[rowCount].appendChild(box)[i];
-            
         }
+        rowCount++;
     }
 }
+function changeColour() {
+    document.getElementsByClassName("box").style.backgroundColour = "green";
+    return false;
+}
+
+grid(numOfSquares);
